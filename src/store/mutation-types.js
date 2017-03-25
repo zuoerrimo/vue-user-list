f27dc3830ad1c8d803ddf9200ceb6ff1c2fe4991
weiyang <weiyang007@yeah.net>
2017-03-25 16:46:23 +0800

    修正

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,16 +1,2 @@
-
 export const SAVE_USER = 'SAVE_USER';
 export const DELETE_USER = 'DELETE_USER';
-// src/store/mutations.js
-import * as types from './mutation-types'
-
-export default {
-  [types.SAVE_USER] (state, user) {
-    state.list.push(
-      user
-    )
-  },
-  [types.DELETE_USER] (state, idx) {
-    state.list.splice(idx, 1);
-  }
-};
